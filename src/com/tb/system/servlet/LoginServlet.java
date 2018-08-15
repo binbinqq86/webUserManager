3484a1371940a2d1a3abbc35d282c57479d15370
田彬 <624715451@qq.com>
2018-08-15 18:04:29 +0800

    update

---
+++ src/com/tb/system/servlet/LoginServlet.java
@@ -0,0 +1,45 @@
+package com.tb.system.servlet;
+
+import com.tb.system.bean.User;
+import com.tb.system.dao.UserDaoImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author tb
+ * @time 2018/8/15 下午4:46
+ * @des 用户登录
+ */
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+        resp.setContentType("text/html;charset=uft-8");
+        System.out.println("********************");
+        //此处对应JSP中的标签名
+        String userName = req.getParameter("userName");
+        String password = req.getParameter("password");
+        HttpSession httpSession = req.getSession();
+        httpSession.setAttribute("userName", userName);
+        httpSession.setAttribute("password", password);
+        List<User> list = UserDaoImpl.getInstance().read(userName);
+        if (list != null && list.size() > 0 && password.equals(list.get(0).password)) {
+            resp.sendRedirect("index.jsp");
+        } else {
+            req.getRequestDispatcher("login.jsp").forward(req, resp);
+        }
+    }
+}
--- web/user/login.jsp
+++ web/user/login.jsp
@@ -9,8 +9,50 @@
 <html>
 <head>
     <title>登录</title>
+
+    <script type="text/javascript">
+        function check() {
+            if (document.getElementById("userName").value == "") {
+                alert("请输入用户名！");
+                return false;
+            }
+            if (document.getElementById("password").value == "") {
+                alert("请输入密码！");
+                return false;
+            }
+        }
+    </script>
 </head>
 <body>
+<h1 align="center">用户登录</h1>
+<form action="LoginServlet" method="post">
+    <table align="center">
+        <tr>
+            <td width="80" align="right">用户名：</td>
+            <td>
+                <input id="userName" type="text" placeholder="请输入用户名"/>
+            </td>
+        </tr>
+        <tr>
+            <td width="80" align="right">密码：</td>
+            <td>
+                <input id="password" type="password" placeholder="请输入密码"/>
+            </td>
+        </tr>
+    </table>
+    <p></p>
+    <div align="center">
+        <input name="login" value="登录" type="submit" class="inputText" style="color: #000000" onclick="return check()">
 
+    </div>
+</form>
 </body>
 </html>
+
+<style>
+    .inputText {
+        height: 30px;
+        width: 60px;
+        background-color: bisque;
+    }
+</style>
